fe457906a3000fdac2908f8fa3d090aa526e9841
Edward Tanguay (windows) <edward@tanguay.info>
2025-01-11 14:30:08 +0100

    displaying number of skills

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -1,19 +1,24 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useTypedStoreState } from "../store/hooks"
 import axios from "axios";
 
 export const PageWelcome = () => {
 	const { message } = useTypedStoreState((state) => state.mainModel);
+	const [skills, setSkills] = useState([]);
 
 	useEffect(() => {
 		(async () => {
 			const response = await axios.get("http://localhost:3355/skills")
-			const skills = response.data
-			console.log(11111, skills);
+			const _skills = response.data
+			setSkills(_skills)
 		})();
 	},[])
 
 	return (
+		<>
 		<p>{message}</p>
+
+			<h2 className="text-2xl mt-3">{skills.length} Skills</h2>
+		</>
 	)
 }
