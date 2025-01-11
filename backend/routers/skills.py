d0c81a14eb15163bc113e3908a6d48f192c5417b
Edward Tanguay (windows) <edward@tanguay.info>
2025-01-11 17:51:25 +0100

    backend delete route works

--- backend/routers/skills.py
+++ backend/routers/skills.py
@@ -1,5 +1,6 @@
 import json
 from fastapi import APIRouter, HTTPException, status
+from fastapi.responses import JSONResponse
 
 router = APIRouter(
 	prefix="/skills"
@@ -21,3 +22,17 @@ async def get_skill(id:int):
 	except:
 		raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="skill not found")
 
+@router.delete("/{id}")
+async def delete_skill(id: int):
+    with open("data/skills.json", "r") as file:
+        skills = json.load(file)
+ 
+    skill_to_delete = next((skill for skill in skills if skill["id"] == id), None)
+    if not skill_to_delete:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Skill not found")
+ 
+    skills = [skill for skill in skills if skill["id"] != id]
+ 
+    with open("data/skills.json", "w") as file:
+        json.dump(skills, file, indent=4)
+    return JSONResponse(content={"deleted": skill_to_delete}, status_code=status.HTTP_200_OK)
