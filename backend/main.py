ab53120a64b40774a4de5e26f8dff5e45e103700
Edward Tanguay (windows) <edward@tanguay.info>
2025-01-11 14:21:59 +0100

    fixed CORS issues

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 from routers import skills
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# CORS
+origins = [
+    "http://localhost:3663"
+]
+app.add_middleware(CORSMiddleware, allow_origins=origins)
 
 app.include_router(skills.router)
 
--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
 import { useTypedStoreState } from "../store/hooks"
+import axios from "axios";
 
 export const PageWelcome = () => {
 	const { message } = useTypedStoreState((state) => state.mainModel);
+
+	useEffect(() => {
+		(async () => {
+			const response = await axios.get("http://localhost:3355/skills")
+			const skills = response.data
+			console.log(11111, skills);
+		})();
+	},[])
 
 	return (
 		<p>{message}</p>
